250a1e0461c75fef3ac4ce0f57688d84f38e56f1
matias_castro <mjcastro@fi.uba.ar>
2026-03-25 13:04:25 -0300

    pulir detalles

--- menu.sh
+++ menu.sh
@@ -15,21 +15,22 @@ echo "Para eliminar el entorno,"
 echo "ejecute el script con el parametro -d"
 echo "Ejemplo: bash menu.sh -d"
 echo "====================================="
- 
-while true
-do
-   echo "      MENU      "
-   echo "1 Crear entorno"
-   echo "2 Correr proceso"
-   echo "3 Listar alumnos"
-   echo "4 Top 10 notas"
-   echo "5 Buscar padron"
-   echo "6 Salir"
-   echo "====================================="
-   
-   read opcion
 
-   case $opcion in
+op=0
+while [ "$op" != "6" ];
+do
+   echo "      MENÚ      "
+   echo "1 Crear entorno."
+   echo "2 Correr proceso."
+   echo "3 Listar alumnos."
+   echo "4 Top 10 notas."
+   echo "5 Buscar padron."
+   echo "6 Salir."
+   echo "====================================="
+
+   read -p "opcion: " op
+
+   case $op in
        1)
            mkdir -p "$ENTRADA"
            mkdir -p "$SALIDA"
@@ -38,32 +39,37 @@ do
            echo "Entorno creado"
            ;;
 
-       2) 
-           if [ -f "$BASE"/consolidar.sh ]; then
-               cd "$BASE"
-               bash consolidar.sh &
-               cd ..
-               echo "Proceso en segundo plano"
-           else 
-               echo "Primero crear entorno"
-           echo "Proceso en segundo plano"
-           fi
+       2)
+	   if [[ -d "$ENTRADA" && -f "$BASE/consolidar.sh" ]]; then
+    		bash "$BASE/consolidar.sh" &
+    		echo "Proceso en segundo plano"
+	   else
+    		echo "No hay entorno creado"
+	   fi
            ;;
 
-       3) 
-           FILE="$SALIDA/$FILENAME.txt"
-           if [ -f "$FILE" ]; then 
-               sort -n "$FILE"
-           else 
-               echo "No existe el archivo"
-           fi
-           ;;
-         
-       4) 
+       3)
            FILE="$SALIDA/$FILENAME.txt"
            if [ -f "$FILE" ]; then
-               sort -k5 -nr "$FILE" | head -10
-           else 
+		if [ -s "$FILE" ]; then
+                    sort -n "$FILE"
+                else
+                    echo "No hay alumnos que listar."
+                fi
+	   else
+		echo "No existe el archivo."
+	   fi
+           ;;
+
+       4)
+           FILE="$SALIDA/$FILENAME.txt"
+           if [ -f "$FILE" ]; then
+		if [ -s "$FILE" ]; then
+		    sort -k5 -nr "$FILE" | head -10
+                else
+                    echo "No hay alumnos que listar."
+		fi
+           else
                echo "No existe el archivo"
            fi
            ;;
@@ -74,7 +80,7 @@ do
                echo "Ingrese padron:"
                read padron
                grep "$padron" "$FILE"
-           else 
+           else
                echo "No existe el archivo"
            fi
            ;;
@@ -87,13 +93,13 @@ do
            echo "Opcion incorrecta: "
            ;;
    esac
-done 
+done
 
 if [ "$1" = "-d" ]; then
    echo "eliminando entorno..."
    rm -rf "$BASE"
    pkill -f consolidar.sh
    echo "Entorno eliminado correctamente"
-else 
+else
    echo "El entorno no fue eliminado "
 fi
